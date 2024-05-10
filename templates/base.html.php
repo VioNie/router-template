f9a2c58593d75dc95a9570bbceb4c1a1df9577e6
MeyDetour <meydetour@gmail.com>
2024-05-10 14:07:02 +0200

    first

--- templates/base.html.php
+++ templates/base.html.php
@@ -6,13 +6,17 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="/css/style.css">
+    <script src="/js/main.js"></script>
 </head>
 <body>
 <header>
     <!-- Ici, mettez votre code HTML pour la navbar -->
     <nav>
         <ul>
-            <li><a href="/">Accueil</a></li>
+            <li><a href="/" class="">Accueil</a></li>
         </ul>
     </nav>
 </header>
@@ -22,5 +26,7 @@
 <footer>
     <p>Copyright © Votre entreprise 2024</p>
 </footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
 </body>
 </html>

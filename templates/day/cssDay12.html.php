a9c568fd1cdc39088ced48569292dc46009e4546
MeyDetour <meydetour@gmail.com>
2024-05-19 10:59:34 +0200

    first

--- templates/day/cssDay12.html.php
+++ templates/day/cssDay12.html.php
@@ -2,15 +2,16 @@
 $day = 12;
 require_once __DIR__ . '/../component/cssDayHeader.html.php' ?>
 <div class="frame12 frame">
-<div class="center center12">
+    <div class="center center12">
         <p>I know quite certainly that I myself have no special talent; curiosity,
-            <span class="d12-word">obsession
+            <span class="d12-word me-1">obsession
             <span class="d12-info">
-
+<span>[əbˈseʃ(ə)n]</span>
+                <span><b>Obsession</b>, a persistent disturbing preoccupation with an often unreasonable idea or feeling.</span>
             </span>
             </span>
             and dogged endurance, combined with self-criticism have brought me to my ideas.</p>
-    <cite>Albert Einstein</cite>
-</div>
+        <cite>Albert Einstein</cite>
+    </div>
 
 </div>

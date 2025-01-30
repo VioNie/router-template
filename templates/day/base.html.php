38d5f3bd31ebe61e0ff5762a1482640566f68d0e
antoineclaitte <claitteantoine@gmail.com>
2025-01-30 23:50:23 +0100

    feat : import css and js

--- templates/day/base.html.php
+++ templates/day/base.html.php
@@ -8,6 +8,8 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/cssday/css1-11.css">
     <!--
     importer le css ici
     example
@@ -15,7 +17,6 @@
     <link rel="stylesheet" href="/css/cssday/css1-11.css">
     <link rel="stylesheet" href="/css/cssday/css12-22.css">
     -->
-
 </head>
 <body>
 <?php require_once __DIR__. '/../component/navbar.html.php'?>
@@ -29,6 +30,10 @@
 Importer les fichier js ici
  <script src="/js/example.js"></script>
  -->
+
+
+<script src="/js/main.js"></script>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </body>
